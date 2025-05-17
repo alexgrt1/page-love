9c4423ef9a21389665e934ccbbf31c9b75c2d83a
alexgrt1 <a.trejo@solucionescomsys.com>
2025-05-16 22:15:37 -0600

    crear back

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// backend-app/server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.get('/api/mensaje', (req, res) => {
+  res.json({ mensaje: '¡Hola soy alexgrt1!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
